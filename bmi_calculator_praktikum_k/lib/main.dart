4b357887bcbaafd9ecbff43810235685fb5efc06
MuhammadFajarRenaldy <muhammadfajarrenaldy@gmail.com>
2021-11-10 20:37:15 +0800

    commit kedua

--- bmi_calculator_praktikum_k/lib/main.dart
+++ bmi_calculator_praktikum_k/lib/main.dart
@@ -4,17 +4,16 @@ import 'input_page.dart';
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
-  const BMICalculator({ Key? key }) : super(key: key);
+  const BMICalculator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: InputPage(),
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0XFF0A0E21),
-        scaffoldBackgroundColor: Color(0XFF0A0E22),
-      )
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E22),
+      ),
     );
   }
 }
-
